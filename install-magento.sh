1fb0d75c51a425b013620f1993d5a1cd346dc0f3
Egor Shitikov <egorshitikov@gmail.com>
2020-07-30 15:47:21 -0700

    Update install-magento.sh

--- install-magento.sh
+++ install-magento.sh
@@ -6,6 +6,6 @@ sudo chmod 777 /var/www/html/magento/
 
 cd /var/www/html/magento/
 
-composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition
+php -d memory_limit=1028M /usr/local/bin/composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition .
 
 sudo chmod 777 /var/www/html/magento/
