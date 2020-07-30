126d6c1dfcdfd8ae35396a91cf32cea685951322
Egor Shitikov <egorshitikov@gmail.com>
2020-07-30 15:14:04 -0700

    Elastic search add

--- install.sh
+++ install.sh
@@ -55,5 +55,21 @@ autorefresh=1
 type=rpm-md
 EOF
 
+sudo yum -y -q install --enablerepo=elasticsearch-7.x elasticsearch
+
+#echo "xpack.security.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*cluster.name.*/cluster.name: magento/" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*node.name.*/node.name: magento-node1/" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*network.host.*/network.host: 127.0.0.1/" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/.*http.port.*/http.port: 9200/" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/-Xms.*/-Xms512m/" /etc/elasticsearch/jvm.options
+sed -i "s/-Xmx.*/-Xmx512m/" /etc/elasticsearch/jvm.options
+chown -R :elasticsearch /etc/elasticsearch/*
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+systemctl restart elasticsearch.service
+#/usr/share/elasticsearch/bin/elasticsearch-setup-passwords auto
+
+curl localhost:9200/
 
 
