fa023975596f11b6cb02f0aaa29ee21cb2d4379a
Egor Shitikov <egorshitikov@gmail.com>
2020-09-10 16:41:24 -0700

    Update download-magento.sh

--- download-magento.sh
+++ download-magento.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+MAGENTO_VERSION="2.3.4"
+
+echo "Download Magento using Composer"
+
 sudo mkdir /var/www/html/magento
 
 sudo chmod 777 /var/www/html/magento/
 
 cd /var/www/html/magento/
 
-php -d memory_limit=1028M /usr/local/bin/composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition .
+php -d memory_limit=1028M /usr/local/bin/composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION .
 
 sudo chmod 777 /var/www/html/magento/
