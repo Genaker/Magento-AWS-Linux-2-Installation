296ae814881c3956a166a99b54d1a87d6bc60fff
Egor Shitikov <egorshitikov@gmail.com>
2020-09-10 16:30:49 -0700

    Create install-composer.sh

---
+++ install-composer.sh
@@ -0,0 +1,8 @@
+#install composer 
+
+echo "Install Composer for PHP"
+
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+composer -v
