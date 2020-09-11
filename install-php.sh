87f322f9384d2c8719ad975cd57d1100d83561c1
Egor Shitikov <egorshitikov@gmail.com>
2020-09-10 22:03:07 -0700

    Update install-php.sh

--- install-php.sh
+++ install-php.sh
@@ -8,3 +8,10 @@ sudo amazon-linux-extras disable php7.4
 sudo amazon-linux-extras install php${PHP_VERSION}
 
 sudo yum install php php-common php-mysqlnd php-opcache php-xml php-mcrypt php-gd php-soap php-redis php-bcmath php-intl php-mbstring php-json php-iconv php-fpm php-apcu php-zip php-devel -y
+
+ sudo yum install php-pear -y
+ sudo yum install libmcrypt libmcrypt-devel -y
+ 
+# sudo pecl install mcrypt-1.0.2 -y
+ 
+ 
