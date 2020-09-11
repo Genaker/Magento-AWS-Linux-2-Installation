3b4468283f67fe4216975b03435d8db6c32b8d9e
Egor Shitikov <egorshitikov@gmail.com>
2020-09-10 17:32:32 -0700

    Update install-php.sh

--- install-php.sh
+++ install-php.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 PHP_VERSION="7.4"
 
+sudo amazon-linux-extras disable php7.4
+
+#sudo amazon-linux-extras disable php7.4
+
 sudo amazon-linux-extras install php${PHP_VERSION}
 
 sudo yum install php php-common php-mysqlnd php-opcache php-xml php-mcrypt php-gd php-soap php-redis php-bcmath php-intl php-mbstring php-json php-iconv php-fpm php-apcu php-zip php-devel -y
