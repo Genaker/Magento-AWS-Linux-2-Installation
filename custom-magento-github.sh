53566893752e2ccf1c112665e8e63cc765363f46
Egor Shitikov <egorshitikov@gmail.com>
2020-09-18 14:12:09 -0700

    Update custom-magento-github.sh

--- custom-magento-github.sh
+++ custom-magento-github.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 YOUR_GIT_REPOSETORY="https://github.com/magento/magento2"
+MAGE_DOMAIN=$(curl ipinfo.io/ip)
 
 cd /var/www/html/magento/
 
@@ -10,7 +11,19 @@ composer install # use instal if you are having Lock file or Update if without
 
 # wget http://s3.example.com/production_dump.sql.gz
 
-#mysql  magento2 < ./production_dump.sql.gz
+# mysql -u root -e  'create database magento2;'
+
+# mysql -u root magento2 < ./production_dump.sql.gz
+
+# zcat ./production_dump.sql.gz | mysql -u root magento2 
+
+# rm /production_dump.sql.gz 
+
+#change magento configurations
+
+# mysql -u root magento2 -e 'UPDATE core_config_data set value = "0" where path like "%web/secure/use_in%"'
+# mysql -u root magento2 -e  "UPDATE core_config_data set value = \"http://$MAGE_DOMAIN/\" where path like \"%base_url%\""
+
+rm -rf ./app/env.php
 
 bash ./install-magento.sh
-
