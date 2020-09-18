e9e14baf2494913d3393f222251354cd577ea2cd
Egor Shitikov <egorshitikov@gmail.com>
2020-09-17 22:15:15 -0700

    Update custom-magento-github.sh

--- custom-magento-github.sh
+++ custom-magento-github.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-git clone https://github.com/magento/magento2 #replace with your git hub url
+YOUR_GIT_REPOSETORY="https://github.com/magento/magento2"
+
+cd /var/www/html/magento/
+
+git clone $YOUR_GIT_REPOSETORY . #replace with your git hub url
+
+composer install # use instal if you are having Lock file or Update if without 
+
+# wget http://s3.example.com/production_dump.sql.gz
+
+#mysql  magento2 < ./production_dump.sql.gz
+
+bash ./install-magento.sh
 
