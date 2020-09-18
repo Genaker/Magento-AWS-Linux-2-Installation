f656e9fcdb732deec01e9f3cd6feae3d479e1922
Egor Shitikov <egorshitikov@gmail.com>
2020-09-18 16:28:49 -0700

    Update custom-magento-github.sh

--- custom-magento-github.sh
+++ custom-magento-github.sh
@@ -7,6 +7,8 @@ cd /var/www/html/magento/
 
 git clone $YOUR_GIT_REPOSETORY . #replace with your git hub url
 
+git config core.fileMode false
+
 composer install # use instal if you are having Lock file or Update if without 
 
 # wget http://s3.example.com/production_dump.sql.gz
