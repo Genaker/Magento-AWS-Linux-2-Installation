95412e81d933f1521fdfca90aff61199687f1dd2
Egor Shitikov <egorshitikov@gmail.com>
2020-09-18 17:26:10 -0700

    add efs mount

---
+++ mount-efs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+EFS_NAME="fs-52168e57"
+
+cd /var/www/html/magento/
+
+sudo yum install -y amazon-efs-utils
+
+mkdir pub/media-tmp
+
+cp -r pub/media/* pub/media-tmp/
+
+sudo mount -t efs -o tls $EFS_NAME:/ pub/media
+
+cp -r pub/media-tmp/* pub/media/  && rm -rf pub/media-tmp/  # beter make async will rur for a while
+
+# or 
+
+# sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport {EFS_DNS}/ efs
+
