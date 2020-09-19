2a240fd925ac985a9c30f654802940d44aaec0b4
Egor Shitikov <egorshitikov@gmail.com>
2020-09-18 17:39:14 -0700

    Update mount-efs.sh

--- mount-efs.sh
+++ mount-efs.sh
@@ -16,6 +16,8 @@ sudo mount -t efs -o tls $EFS_NAME:/ pub/media
 
 cp -r pub/media-tmp/* pub/media/  && rm -rf pub/media-tmp/  # beter make async will rur for a while
 
+sudo chmod -r 777 pub/media/ 
+
 # or 
 
 # sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport {EFS_DNS}/ efs
