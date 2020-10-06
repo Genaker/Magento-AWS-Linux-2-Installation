e514d34e1770503f9fbc3ffc9770d01ab5a52651
Egor Shitikov <egorshitikov@gmail.com>
2020-10-06 16:14:54 -0700

    Create install-magepack.sh

---
+++ install-magepack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo npm install -g magepack --unsafe-perm=true --allow-root
+
+sudo yum install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 -y
+
