9b0ba9b619bf85754a507bc5a06b992c1707ed21
Egor Shitikov <egorshitikov@gmail.com>
2020-10-06 22:17:57 -0700

    compile redis

--- install-redis.sh
+++ install-redis.sh
@@ -2,21 +2,49 @@
 
 echo "Install Redis"
 
-sudo amazon-linux-extras install epel -y 
+sudo yum install gcc -y
 
-sudo yum install epel-release yum-utils -y
+wget http://download.redis.io/redis-stable.tar.gz 
 
-sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+tar xvzf redis-stable.tar.gz
 
-sudo yum-config-manager --enable remi
+cd ./redis-stable/deps
+make hiredis jemalloc linenoise lua geohash-int
+cd ..
+make
+sudo make install
 
-sudo yum install redis -y
+sudo mkdir -p /etc/redis
 
-redis-server -v
+cd ..
 
-sudo service redis start
+pwd
 
-sudo chkconfig redis on
-# edit redis configuration ToDO:
-#sudo nano /etc/redis.conf
+sudo cp ./redis-stable/redis.conf /etc/redis
+
+#sudo -u ec2-user /usr/local/bin/redis-server --daemonize yes
+
+# Run as SUDO
+
+cat > /etc/systemd/system/redis.service <<END
+[Unit]
+Description=Redis Data Store
+After=network.target
+
+[Service]
+User=ec2-user
+Group=ec2-user
+ExecStart=/usr/local/bin/redis-server /etc/redis/redis.conf
+ExecStop=/usr/local/bin/redis-cli shutdown
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END
+
 sudo systemctl enable redis
+sudo systemctl start redis
+
+sudo systemctl status redis
+
+sudo service redis status
