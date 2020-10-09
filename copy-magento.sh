cf9b8d83e579fbeb1ac009a82c91ca4d9d4a08ac
Egor Shitikov <egorshitikov@gmail.com>
2020-10-08 18:55:59 -0700

    Update copy-magento.sh

--- copy-magento.sh
+++ copy-magento.sh
@@ -18,7 +18,13 @@ sudo tar -xzf magento.tar.gz -C magento
 
 
 # SET: AUTOCOMMIT=0, UNIQUE_CHECKS=0, FOREIGN_KEY_CHECKS=0 (AND DON'T FORGET TO ROLLBACK THIS CHANGES)
-mysqladmin -h  -u   create database 'magento2'
+mysqladmin -h  -u  create 'magento2'
 
 mysql -h -u -p magento2 < magento_dump.sql
+
+## I also had an issue :
+
+# ERROR 1293 (HY000) at line 142: Incorrect table definition; there can be only one TIMESTAMP column with CURRENT_TIMESTAMP in DEFAULT or ON UPDATE clause
+# fix
+# sed -i 's/timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT /timestamp NOT NULL DEFAULT 0 COMMENT /' magento_dump.sql
 
