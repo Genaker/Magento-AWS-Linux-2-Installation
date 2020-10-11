19ff521c06434fd07e266a3fafa6e97d43f7639b
Egor Shitikov <egorshitikov@gmail.com>
2020-10-11 16:25:47 -0700

    Create install-php-docker.pp

---
+++ install-php-docker.pp
@@ -0,0 +1,11 @@
+# ideo of this repo is not to use Docker for the basic functionality 'php,nginx,redis'. Ide is to be close to the corre and don't use aditional level of complexity and bugs 
+
+
+docker pull php:7.3
+
+#connect to the docker 
+docker run -it --net=host  php:7.3 bash
+
+# inside of the docker 
+apt-get update -y
+apt install nano -v
