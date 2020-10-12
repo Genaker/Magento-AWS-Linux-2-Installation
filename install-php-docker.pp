73dcbea520ed42958792483666cb13bf77fc00c0
Egor Shitikov <egorshitikov@gmail.com>
2020-10-11 18:07:05 -0700

    Update install-php-docker.pp

--- install-php-docker.pp
+++ install-php-docker.pp
@@ -1,5 +1,14 @@
 # ideo of this repo is not to use Docker for the basic functionality 'php,nginx,redis'. Ide is to be close to the corre and don't use aditional level of complexity and bugs 
 
+sudo yum install docker -y
+sudo service docker start
+sudo systemctl enable docker
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+newgrp docker
 
 docker pull php:7.3
 
@@ -8,4 +17,4 @@ docker run -it --net=host  php:7.3 bash
 
 # inside of the docker 
 apt-get update -y
-apt install nano -v
+apt install nano -y
