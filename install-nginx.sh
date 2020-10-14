70a2874b22d198823d4e7d59ea614f027756bbd8
Egor Shitikov <egorshitikov@gmail.com>
2020-10-14 00:52:36 -0700

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Official documentation how to install 
+# https://docs.nginx.com/nginx/admin-guide/installing-nginx/installing-nginx-open-source/#installing-a-prebuilt-centos-rhel-package-from-the-official-nginx-repository
 echo "Install Nginx"
 
 sudo amazon-linux-extras install nginx1 -y
