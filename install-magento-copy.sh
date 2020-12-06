186a08d0834e43d2a622b6d844dcc36df50bc019
Egor Shitikov <egorshitikov@gmail.com>
2020-12-06 02:51:30 -0800

    Update install-magento-copy.sh

--- install-magento-copy.sh
+++ install-magento-copy.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+## on remote original server 
+
+# mysqldump -h production.cluster.us-east-1.rds.amazonaws.com -u prod_M2 -p'' magento2 | gzip -c | cat > myMagentoDatabaseDump-$(date +%Y-%m-%d-%H.%M.%S).sql.gz
+
+## download form remote 
+
+# scp remoteuser@remoteserver:~/myMagentoDatabaseDump.txt  myMagentoDatabaseDump.sql.gz
+
 cd {path to magento}
 
-tar -pczf magento.tar.gz --warning=no-file-changed --exclude "*/pub/media/*" .
+## on a server archive magento forlder 
+# tar -pczf magento.tar.gz --warning=no-file-changed --exclude "*/pub/media/*" .
+
+# scp remoteuser@remoteserver:~/magento.tar.gz  magento.tar.gz
 
 cp magento.tar.gz pub/media/
 
 wget http://{{PATH}}/media/magento.zip
 
-tar -xzf magento.zip
+mkdir -p  /var/www/html/magento/
+
+tar -xzf magento.zip --directory /var/www/html/magento/
+
+## gzip -d magento.zip
 
 mysql -h {host} -u {user} -p'{password}' {db_name} | gzip > mysqldump.sql.gz
 
