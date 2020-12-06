cdc2056a97df28aaa48f6fcc23dcc4f5e9a25cc1
Egor Shitikov <egorshitikov@gmail.com>
2020-12-06 02:12:00 -0800

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -4,25 +4,25 @@ set -x
 
 echo "Install PHP \n"
 
-./install-php.sh
-./configure-php.sh
-./install-tideways-profiler.sh
+bash ./install-php.sh
+bash ./configure-php.sh
+bash ./install-tideways-profiler.sh
 
 echo "Install NGINX \n"
 
-./install-nginx.sh
+bash ./install-nginx.sh
 
 echo "Install Redis \n"
 
-./install-redis-compile.sh
+bash ./install-redis-compile.sh
 
 echo "Install Nginx \n"
 
-./install-nginx.sh
+bash ./install-nginx.sh
 
 echo "Install MYSQL/MARIA DB \n"
 
-./install-maria-db-aws-extras.sh
+bash ./install-maria-db-aws-extras.sh
 
 #echo "Install Elastic Search \n"
 
