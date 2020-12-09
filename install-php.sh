8c8e160182b6a15340ad8f234c16ab0b393de8b2
Egor Shitikov <egorshitikov@gmail.com>
2020-12-09 12:37:18 -0800

    Update install-php.sh

--- install-php.sh
+++ install-php.sh
@@ -16,6 +16,7 @@ then
   sudo amazon-linux-extras enable php${PHP_VERSION}
   # to downgarede use disable command
   #sudo amazon-linux-extras disable php7.4 
+  # sudo yum autoremove php php-common
   sudo amazon-linux-extras install php${PHP_VERSION} 
   # sudo yum install php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
   OS_RELATED = " php-redis php-pear libmcrypt libmcrypt-devel php-mcrypt "
