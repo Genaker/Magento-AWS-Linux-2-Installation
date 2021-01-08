0d88b76eefc48c93378b825f74331a13a1a13bfa
Egor Shitikov <egorshitikov@gmail.com>
2021-01-08 07:38:42 -0800

    Create install-mysql-percona.sh

---
+++ install-mysql-percona.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO_PERCONA="https://repo.percona.com/yum/percona-release-latest.noarch.rpm"
+sudo yum install -y -q $REPO_PERCONA
+echo "Percona 8.0 database installation:\n"
+dnf module disable -y mysql
+
+# https://www.percona.com/blog/2019/05/31/rhel-8-packages-available-for-percona-products/
+
+percona-release setup ps57
+sudo dnf install Percona-Server-server-57 Percona-Server-client-57
+
+  
