20ae423c1d1590b375878122eea3b00132c608b8
Egor Shitikov <egorshitikov@gmail.com>
2021-01-11 00:37:24 -0800

    Update env.php

--- configs/env.php
+++ configs/env.php
@@ -1,1 +1,89 @@
 <?php
+return [
+    'backend' => [
+        'frontName' => 'admin_admin'
+    ],
+    'db' => [
+        'connection' => [
+            'indexer' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'persistent' => null
+            ],
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ],
+        'table_prefix' => ''
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '6ef1f56fae6e3ff11cf0560eb60e6aa8'
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'def_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'def_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'target_rule' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'google_product' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Mon, 11 Jan 2021 08:35:24 +0000'
+    ]
+];
