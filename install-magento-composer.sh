52cdce3edebddb391d8779b069f46997707212ae
Egor Shitikov <egorshitikov@gmail.com>
2021-02-08 19:35:33 -0800

    Update install-magento-composer.sh

--- install-magento-composer.sh
+++ install-magento-composer.sh
@@ -12,6 +12,6 @@ sudo chmod 777 /var/www/html/magento/
 
 cd /var/www/html/magento/
 
-php -d memory_limit=1028M /usr/local/bin/composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION .
+php -d memory_limit=1028M /usr/local/bin/composer create-project --ignore-platform-reqs --repository-url=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION . 
 
 sudo chmod 777 /var/www/html/magento/
