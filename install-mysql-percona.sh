6fdbe309af45579966ec6c56838f0640b9d8baf2
Egor Shitikov <egorshitikov@gmail.com>
2021-02-08 19:33:36 -0800

    Update install-mysql-percona.sh

--- install-mysql-percona.sh
+++ install-mysql-percona.sh
@@ -8,7 +8,7 @@ dnf module disable -y mysql
 # https://www.percona.com/blog/2019/05/31/rhel-8-packages-available-for-percona-products/
 
 percona-release setup ps57
-sudo dnf install Percona-Server-server-57 Percona-Server-client-57
+sudo dnf install Percona-Server-server-57 Percona-Server-client-57 -y
 
 echo "MySQL ROOT Password:\n"
 cat /var/log/mysqld.log |grep generated
@@ -16,8 +16,3 @@ cat /var/log/mysqld.log |grep generated
 systemctl enable mysqld 
 
 systemctl start mysqld.service
-
-
-
-
-  
