3b59f8046dfa76147e4ebbbdd5140e61d0e530da
Egor Shitikov <egorshitikov@gmail.com>
2021-02-21 11:43:27 -0800

    Create configure-swap.sh

---
+++ configure-swap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo fallocate -l 1G /swapfile
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo nano /etc/fstab
+echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
