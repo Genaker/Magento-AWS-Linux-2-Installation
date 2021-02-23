6319ab676b8944adced20ed43f521e3b8ec80400
Egor Shitikov <egorshitikov@gmail.com>
2021-02-23 00:23:08 -0800

    Update configure-swap.sh

--- configure-swap.sh
+++ configure-swap.sh
@@ -5,5 +5,5 @@ sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
-sudo nano /etc/fstab
+#sudo nano /etc/fstab
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
