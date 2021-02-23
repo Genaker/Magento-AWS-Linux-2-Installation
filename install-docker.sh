c12170249c9dfe64aefe82e852456682fd994ada
Egor Shitikov <egorshitikov@gmail.com>
2021-02-23 00:39:21 -0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -3,3 +3,17 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ec2-user
+
+# Issue is ip table is not installed
+yum install iptables-services -y
+
+sudo service docker start
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compos
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Test Docker Compose
+docker-compose --version
