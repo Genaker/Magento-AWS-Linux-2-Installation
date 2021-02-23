10d7ef83c03f05b55c21f6b0c1f9a7b1b1e8c442
Egor Shitikov <egorshitikov@gmail.com>
2021-02-23 00:23:51 -0800

    Update configure-swap.sh

--- configure-swap.sh
+++ configure-swap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo fallocate -l 1G /swapfile
+sudo fallocate -l 2G /swapfile
 sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
