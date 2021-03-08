ef73b7322f534db81adbc767dcb940a5031ca071
Egor Shitikov <egorshitikov@gmail.com>
2021-03-07 22:42:12 -0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -12,7 +12,7 @@ sudo systemctl enable docker.service
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compos
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
