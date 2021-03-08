3de41436127b900b31e6eda81995461f454b9e1c
Egor Shitikov <egorshitikov@gmail.com>
2021-03-07 22:00:13 -0800

    Update install-mysql-repo.sh

--- install-mysql-repo.sh
+++ install-mysql-repo.sh
@@ -21,3 +21,7 @@ sudo yum -y install MariaDB-server MariaDB-client
 sudo service mariadb start
 
 mysql -e 'Select Version()';
+
+## Check if Mysql is Running 
+
+systemctl is-active --quiet mysql  ||  (mkdir -p /var/run/mysqld/ && sudo chmod -R 777 /var/run/mysqld/ && sudo service mysql restart)
