6e79db93049bbb6274407d4da8baa1b19188938e
Egor Shitikov <egorshitikov@gmail.com>
2021-03-17 00:17:31 -0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 curl -fsSL https://get.docker.com -o get-docker.sh 
 sudo sh get-docker.sh || (sudo amazon-linux-extras install docker && sudo yum install docker -y && sudo usermod -a -G docker ec2-user)
 sudo usermod -aG docker $USER
