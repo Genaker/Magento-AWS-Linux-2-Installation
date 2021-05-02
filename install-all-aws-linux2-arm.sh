dc0fcad9aa0f10f45eac34332948465b6079bd6c
Egor Shitikov <egorshitikov@gmail.com>
2021-05-02 16:51:00 -0700

    Create install-all-aws-linux2-arm.sh

---
+++ install-all-aws-linux2-arm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -x
+
+echo "Install Necessary software:\n"
+
+bash ./install-prepare.sh
+
+echo "Install PHP \n"
+
+bash ./install-php.sh
+bash ./configure-php.sh
+bash ./install-tideways-profiler.sh
+bash ./install-composer.sh
+
+echo "Install NGINX \n"
+
+bash ./install-nginx.sh
+
+echo "Install Redis \n"
+
+bash ./install-redis-compile.sh
+
+echo "Configure Nginx \n"
+
+bash ./configure-nginx.sh
+
+echo "Install MYSQL/MARIA DB \n"
+
+bash ./install-mariadb-docker.sh
+
+echo "Install Elastic Search \n"
+
+./install-elastic-search.sh
+
+echo "Install Magento \n"
+
+bash ./install-monorepo-git.sh
+
+echo "Install Magento \n"
+
+bash ./install-monorepo-git.sh
+
+echo "Configure Nginx \n"
+
+bash ./configure-nginx.sh
