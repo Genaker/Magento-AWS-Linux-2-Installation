ca3d3a6bd4646feb2f1e84db6e4f49564b23bc01
Egor Shitikov <egorshitikov@gmail.com>
2021-05-02 17:23:55 -0700

    Update install-monorepo-git.sh

--- install-monorepo-git.sh
+++ install-monorepo-git.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkdir -p /var/www/html/magento/
+
 cd /var/www/html/magento/
 
 git clone https://github.com/magenx/Magento-2.git .
 
-echo "\nRun: bin/magento  setup:install\n"
+echo "\nRun: "bin/magento  setup:install" to finish installation\n"
