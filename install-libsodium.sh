5b3fe54fdabcf5c221685883218483075b4b1101
Egor Shitikov <egorshitikov@gmail.com>
2021-05-02 17:11:30 -0700

    Update install-libsodium.sh

--- install-libsodium.sh
+++ install-libsodium.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # Install Lib Sodium PHP
+if echo $LINUX_VERSION | grep -q "Amazon Linux release 2"
+then
+
+sudo amazon-linux-extras install epel
+
+fi
 
 pecl install -f libsodium
 
@@ -10,7 +16,7 @@ sudo pecl install libsodium
 
 sudo yum --enablerepo="power*" --enablerepo="epel"  install php-sodium
 
-echo "extension=sodium.so" >> /etc/php.ini
+# echo "extension=sodium.so" >> /etc/php.ini
 
 sudo service php-fpm restart
 
