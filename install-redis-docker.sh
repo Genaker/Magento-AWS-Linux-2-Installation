63945175559186205d28bc5bb886344b293c8ea8
Egor Shitikov <egorshitikov@gmail.com>
2021-06-13 18:52:34 -0700

    Create install-redis-docker.sh

---
+++ install-redis-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo bash ./install-docker-service.sh
+
+docker pull redis
+
+docker images
+
+# ToDo: volume my.conf and db files
+# Official Documentatio https://hub.docker.com/_/mariadb
+
+sudo docker run --name redis -p 6379:6379 --restart unless-stopped -d redis:alpine3.13 --save ''
+
+# mysql-cli
+
+sleep 5
+
+docker exec -it redis redis-cli -v
+
+docker exec -it redis redis-cli ping
+
+docker ps
+
