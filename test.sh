8466089ec92d59b56275baf6b89224a466813e16
Egor Shitikov <egorshitikov@gmail.com>
2021-06-24 10:34:48 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-sudo service nginx status ; sudo service redis status ; sudo service elasticseach status ; sudo service docker status ; sudo service php-fpm status ; docker ps ;
+sudo service nginx status ; sudo systemctl is-active --quiet redis && echo "Redis is running" ; sudo service elasticsearch status; sudo service docker status ; sudo service php-fpm status ; docker ps ;
