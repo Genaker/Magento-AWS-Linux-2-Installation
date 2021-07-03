15915e9249bb0e367d53da82bea7f32d20cc00e8
Egor Shitikov <egorshitikov@gmail.com>
2021-07-03 13:47:48 -0700

    Update install-wordpress-all.sh

--- install-wordpress-all.sh
+++ install-wordpress-all.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
+
+set -x
+
+echo "Installing Utilities \n"
+
+bash ./install-prepare.sh
+
+echo "Install PHP \n"
+
+bash ./install-php.sh
+bash ./configure-php.sh
+# bash ./install-tideways-profiler.sh
+
+set +e
+bash ./install-composer.sh
+set -e
+
+echo "Install NGINX \n"
+
+bash ./install-nginx.sh
+
+echo "Configure Nginx \n"
+
+echo "Install Redis \n"
+
+bash ./install-redis-compile.sh
+
+echo "Install MYSQL/MARIA DB \n"
+
+bash ./install-mariadb-docker.sh
+
 
