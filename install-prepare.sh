7f15a679a745e5aaeb1a67977338870587e3785f
Egor Shitikov <egorshitikov@gmail.com>
2021-07-03 14:13:30 -0700

    Update install-prepare.sh

--- install-prepare.sh
+++ install-prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## Prepare docker to install Magento software
-
+LINUX_VERSION=$(cat /etc/system-release)
 yum install -y epel-release oracle-epel-release-el8 --skip-broken 
 
 if echo $LINUX_VERSION | grep -q "Oracle Linux Server release 8"
@@ -8,9 +8,6 @@ then
   # TO DO MOVE REPOS to THE initial SCRITt 
   
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
- 
-else
-
 fi
 
 yum install -y htop nano sudo git initscripts wget unzip mysql
