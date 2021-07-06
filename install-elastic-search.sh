5724d8506e601002bd312a042e11476d7a8f21bc
Egor Shitikov <egorshitikov@gmail.com>
2021-07-06 14:57:22 -0700

    Update install-elastic-search.sh

--- install-elastic-search.sh
+++ install-elastic-search.sh
@@ -20,13 +20,14 @@ EOF
 
 sudo yum -y install --enablerepo=elasticsearch-${ELKREPO} elasticsearch
 
+echo "discovery.type: single-node" >> /etc/elasticsearch/elasticsearch.yml
 #echo "xpack.security.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
 sed -i "s/.*cluster.name.*/cluster.name: magento/" /etc/elasticsearch/elasticsearch.yml
 sed -i "s/.*node.name.*/node.name: magento-node1/" /etc/elasticsearch/elasticsearch.yml
 sed -i "s/.*network.host.*/network.host: 127.0.0.1/" /etc/elasticsearch/elasticsearch.yml
 sed -i "s/.*http.port.*/http.port: 9200/" /etc/elasticsearch/elasticsearch.yml
-sed -i "s/-Xms.*/-Xms2024m/" /etc/elasticsearch/jvm.options
-sed -i "s/-Xmx.*/-Xmx2024m/" /etc/elasticsearch/jvm.options
+sed -i "s/.*-Xms.*/-Xms888m/" /etc/elasticsearch/jvm.options
+sed -i "s/.*-Xms.*/-Xms888m/" /etc/elasticsearch/jvm.options
 chown -R :elasticsearch /etc/elasticsearch/*
 
 systemctl daemon-reload
