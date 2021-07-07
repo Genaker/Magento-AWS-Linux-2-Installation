8fca80b352e44e347b5551626f4d48b8defa4b42
Egor Shitikov <egorshitikov@gmail.com>
2021-07-07 00:39:30 -0700

    Update install-mariadb-repo.sh

--- install-mariadb-repo.sh
+++ install-mariadb-repo.sh
@@ -1,9 +1,36 @@
 #!/bin/bash/
 # Repo is not working with ARM instances
-wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
-sudo chmod +x mariadb_repo_setup
+#wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
+#sudo chmod +x mariadb_repo_setup
 
-sudo ./mariadb_repo_setup
+#sudo ./mariadb_repo_setup
+cat > /etc/yum.repos.d/mariadb.repo << EOF
+[mariadb-main]
+name = MariaDB Server
+baseurl = https://dlm.mariadb.com/repo/mariadb-server/10.4/yum/rhel/8/x86_64
+gpgkey = file:///etc/pki/rpm-gpg/MariaDB-Server-GPG-KEY
+gpgcheck = 1
+enabled = 1
+module_hotfixes = 1
+
+[mariadb-maxscale]
+# To use the latest stable release of MaxScale, use "latest" as the version
+# To use the latest beta (or stable if no current beta) release of MaxScale, use "beta" as the version
+name = MariaDB MaxScale
+baseurl = https://dlm.mariadb.com/repo/maxscale/latest/yum/rhel/8/x86_64
+gpgkey = file:///etc/pki/rpm-gpg/MariaDB-MaxScale-GPG-KEY
+gpgcheck = 1
+enabled = 1
+
+[mariadb-tools]
+name = MariaDB Tools
+baseurl = https://downloads.mariadb.com/Tools/rhel/8/x86_64
+gpgkey = file:///etc/pki/rpm-gpg/MariaDB-Enterprise-GPG-KEY
+gpgcheck = 1
+enabled = 1
+EOF
+
+sed -i 's/80/88/g'  /etc/nginx/nginx.conf
 
 sudo yum remove mysql* -y
 
