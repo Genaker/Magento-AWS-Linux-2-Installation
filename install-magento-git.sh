6b3a931888531030e4255b160bd18d0789e366ff
Egor Shitikov <egorshitikov@gmail.com>
2021-07-06 22:36:07 -0700

    Update install-magento-git.sh

--- install-magento-git.sh
+++ install-magento-git.sh
@@ -12,11 +12,13 @@ echo "\nRun: "bin/magento  setup:install" to finish installation\n"
 
 mysql -h 127.0.0.1 -u root  -p'root' -e 'create database magento';
 
-IP=$(wget -qO - icanhazip.com)
+IP_REAL=$(wget -qO - icanhazip.com)
+IP=${IP:=IP_REAL}
+MYSQL_DB_PASS=${MYSQL_DB_PASS:=root}
 
 sudo service nginx restart
 
-php -d memory_limit=5028M /var/www/html/magento/bin/magento setup:install --base-url=http://$IP --base-url-secure=http://$IP --use-secure=0 --use-rewrites=1 --use-secure-admin=1 --backend-frontname=admin --db-host=127.0.0.1 --db-name=magento --db-user=root --db-password=root --use-secure=0 --base-url-secure=0 --use-secure-admin=0 --admin-firstname=Admin --admin-lastname=Adminovich --admin-email=admin@admin.ua --admin-user=admin --backend-frontname=admin --use-rewrites=1 --admin-password=admin123
+php -d memory_limit=5028M /var/www/html/magento/bin/magento setup:install --base-url=http://$IP --base-url-secure=http://$IP --use-secure=0 --use-rewrites=1 --use-secure-admin=1 --backend-frontname=admin --db-host=127.0.0.1 --db-name=magento --db-user=root --db-password=$MYSQL_DB_PASS --use-secure=0 --base-url-secure=0 --use-secure-admin=0 --admin-firstname=Admin --admin-lastname=Adminovich --admin-email=admin@admin.ua --admin-user=admin --backend-frontname=admin --use-rewrites=1 --admin-password=admin123
 
 echo "chmod -R 777 /var/www/html/magento/"
 
