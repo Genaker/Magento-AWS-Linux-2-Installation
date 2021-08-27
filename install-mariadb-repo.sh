ff0af3e7444347b585ca3736f548e5a21a44f48c
Egor Shitikov <egorshitikov@gmail.com>
2021-08-27 15:09:51 -0700

    Update install-mariadb-repo.sh

--- install-mariadb-repo.sh
+++ install-mariadb-repo.sh
@@ -1,10 +1,19 @@
 #!/bin/bash/
 # Repo is not working with ARM instances
-wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
-sudo chmod +x mariadb_repo_setup
 
-sudo ./mariadb_repo_setup
-rm /etc/yum.repos.d/mariadb.repo
+if echo $LINUX_VERSION | grep -q "CentOS Linux release 8"
+then
+##wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
+
+# The MariaDB Repository supports these Linux OSs, on x86_64 only:
+#    * RHEL/CentOS 7 & 8 (rhel)
+#    * Ubuntu 18.04 LTS (bionic), & 20.04 LTS (focal)
+#    * Debian 9 (stretch), 10 (buster), & 11 (bullseye)
+#    * SLES 12 & 15 (sles)
+
+##sudo chmod +x mariadb_repo_setup
+##sudo ./mariadb_repo_setup
+##rm /etc/yum.repos.d/mariadb.repo
 
 cat > /etc/yum.repos.d/mariadb.repo << EOF
 [mariadb-main]
@@ -39,5 +48,15 @@ sudo yum install perl-DBI libaio libsepol lsof boost-program-options rsync -y
 sudo yum install MariaDB-server -y
 
 sudo systemctl start mariadb.service
-
 sudo systemctl enable mariadb.service
+
+
+## Oracle HAS OWN MYSQL not MARIADB repo with ARM support 
+
+elif echo $LINUX_VERSION | grep -q "Oracle Linux Server release 8"
+than
+echo "Installing MySQL 8 and not MariaDB"
+sudo yum install mysql -y
+sudo service mysqld start
+sudo systemctl enable mysqld
+fi
