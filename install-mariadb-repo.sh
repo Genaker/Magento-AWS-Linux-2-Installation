c23c2cd073bfa27aba27bd1f287acf96152fe22d
Egor Shitikov <egorshitikov@gmail.com>
2021-08-27 16:31:38 -0700

    Update install-mariadb-repo.sh

--- install-mariadb-repo.sh
+++ install-mariadb-repo.sh
@@ -67,7 +67,7 @@ sudo pkill mysql
 sudo  mysqld --skip-grant-tables --user=mysql &
 sleep 10
 mysql -h 127.0.0.1 -u root -e "FLUSH PRIVILEGES;ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'"
-sudo pkill mysql
+sudo pkill mysqld
 sudo service mysqld start
 sudo systemctl enable mysqld
 mysql -h 127.0.0.1 -u root -p'$MYSQL_PASSWORD' -e 'select VERSION();'
