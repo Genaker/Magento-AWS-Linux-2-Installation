b32613ff4c4c9d89b9e4843a9a3a3a54d5e97211
Egor Shitikov <egorshitikov@gmail.com>
2021-08-29 20:42:35 -0700

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -2,7 +2,7 @@
 #!bin/bash
 
 wget https://github.com/Genaker/Magento-AWS-Linux-2-Installation/archive/refs/heads/master.zip
-unzip magento.zip
+unzip master.zip
 cd Magento-AWS-Linux-2-Installation-master/
 sudo bash ./install-all.sh
 
