774f73bedae0e5261750f7f5807e826787eec3de
Egor Shitikov <egorshitikov@gmail.com>
2021-10-07 14:22:13 -0700

    Update install-elasticsearch-docker.sh

--- install-elasticsearch-docker.sh
+++ install-elasticsearch-docker.sh
@@ -4,5 +4,5 @@ ELASTIC_VERSION='7.13.2'
 
 echo "Install Elastic SEARCH Docker"
 
-docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:$ELASTIC_VERSION
+docker run -d --restart unless-stopped -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:$ELASTIC_VERSION
 
