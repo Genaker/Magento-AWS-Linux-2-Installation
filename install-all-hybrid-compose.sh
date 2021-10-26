4860f9807151a8cb5c6898a730147ab1dfc40243
Egor Shitikov <egorshitikov@gmail.com>
2021-10-25 23:31:05 -0400

    Update install-all-hybrid-compose.sh

--- install-all-hybrid-compose.sh
+++ install-all-hybrid-compose.sh
@@ -33,9 +33,24 @@ echo "Install Doker Compose ElasticSearch/MariaDB/Redis containers"
 
 docker-compose -f magento-docker-compose-hybrid.yml up -d
 
+# Password from the docker compose 
+export DB_PASSWORD=lfsdkgj@etwe456otpreDSFdsf34rer
+
 docker ps
 
 curl 127.0.0.1:9200
 
 docker exec -it redis redis-cli -r 1 info server
 mysql -h 127.0.0.1 -u root -p'sdsd' -c'version()'
+
+# install magento 
+
+echo "Install Magento"
+export MAGENTO_VERSION="2.4.3-p1"
+sudo mkdir -p /var/www/html/magento
+sudo chmod 777 /var/www/html/magento/
+cd /var/www/html/magento/
+php -d memory_limit=5028M /usr/local/bin/composer create-project --ignore-platform-reqs --repository-url=https://repo.magento.com/ magento/project-community-edition=$MAGENTO_VERSION . 
+sudo chmod 777 /var/www/html/magento/
+
+sudo bash ./install-magento.sh
