f7c51c2ee7fe49e6d523dbc1acf282b15157e166
Egor Shitikov <egorshitikov@gmail.com>
2023-03-15 20:15:20 -0700

    Update install-composer.sh

--- install-composer.sh
+++ install-composer.sh
@@ -5,7 +5,7 @@
 echo "Install Composer for PHP"
 
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer --version=1.10.17
+sudo php composer-setup.php --install-dir=/usr/bin --filename=composer --version>=2.5.4
 
 composer config --global http-basic.repo.magento.com 5310458a34d580de1700dfe826ff19a1 255059b03eb9d30604d5ef52fca7465d
 
